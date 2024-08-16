2b51427e8edadab61dba65f418336251aa384876
Tran Van Bao <133487627+TranVanBao1411@users.noreply.github.com>
2024-08-16 13:13:24 +0700

    Update InterfaceManager.lua

--- InterfaceManager.lua
+++ InterfaceManager.lua
@@ -1,40 +1,40 @@
 local httpService = game:GetService("HttpService")
 
 local InterfaceManager = {} do
-	InterfaceManager.Folder = "FluentSettings"
+    InterfaceManager.Folder = "FluentSettings"
     InterfaceManager.Settings = {
-        Theme = "Darker",
+        Theme = "Dark",
         Acrylic = true,
         Transparency = true,
     }
 
     function InterfaceManager:SetFolder(folder)
-		self.Folder = folder
-		self:BuildFolderTree()
-	end
+        self.Folder = folder;
+        self:BuildFolderTree()
+    end
 
     function InterfaceManager:SetLibrary(library)
-		self.Library = library
-	end
+        self.Library = library
+    end
 
     function InterfaceManager:BuildFolderTree()
-		local paths = {}
+        local paths = {}
 
-		local parts = self.Folder:split("/")
-		for idx = 1, #parts do
-			paths[#paths + 1] = table.concat(parts, "/", 1, idx)
-		end
+        local parts = self.Folder:split("/")
+        for idx = 1, #parts do
+            paths[#paths + 1] = table.concat(parts, "/", 1, idx)
+        end
 
-		table.insert(paths, self.Folder)
-		table.insert(paths, self.Folder .. "/settings")
+        table.insert(paths, self.Folder)
+        table.insert(paths, self.Folder .. "/settings")
 
-		for i = 1, #paths do
-			local str = paths[i]
-			if not isfolder(str) then
-				makefolder(str)
-			end
-		end
-	end
+        for i = 1, #paths do
+            local str = paths[i]
+            if not isfolder(str) then
+                makefolder(str)
+            end
+        end
+    end
 
     function InterfaceManager:SaveSettings()
         writefile(self.Folder .. "/options.json", httpService:JSONEncode(InterfaceManager.Settings))
@@ -56,51 +56,51 @@ local InterfaceManager = {} do
 
     function InterfaceManager:BuildInterfaceSection(tab)
         assert(self.Library, "Must set InterfaceManager.Library")
-		local Library = self.Library
+        local Library = self.Library
         local Settings = InterfaceManager.Settings
 
         InterfaceManager:LoadSettings()
 
-		local section = tab:AddSection("Interface")
+        local section = tab:AddSection("Interface")
 
-		local InterfaceTheme = section:AddDropdown("InterfaceTheme", {
-			Title = "Themes",
-			Description = "",
-			Values = Library.Themes,
-			Default = Settings.Theme,
-			Callback = function(Value)
-				Library:SetTheme(Value)
+        local InterfaceTheme = section:AddDropdown("InterfaceTheme", {
+            Title = "Themes",
+            Description = "",
+            Values = Library.Themes,
+            Default = Settings.Theme,
+            Callback = function(Value)
+                Library:SetTheme(Value)
                 Settings.Theme = Value
                 InterfaceManager:SaveSettings()
-			end
-		})
+            end
+        })
 
         InterfaceTheme:SetValue(Settings.Theme)
-	
-		if Library.UseAcrylic then
-			section:AddToggle("AcrylicToggle", {
-				Title = "Acrylic",
-				Description = "",
-				Default = Settings.Acrylic,
-				Callback = function(Value)
-					Library:ToggleAcrylic(Value)
+
+        if Library.UseAcrylic then
+            section:AddToggle("AcrylicToggle", {
+                Title = "Acrylic",
+                Description = "",
+                Default = Settings.Acrylic,
+                Callback = function(Value)
+                    Library:ToggleAcrylic(Value)
                     Settings.Acrylic = Value
                     InterfaceManager:SaveSettings()
-				end
-			})
-		end
-	
-		section:AddToggle("TransparentToggle", {
-			Title = "Transparency",
-			Description = "",
-			Default = Settings.Transparency,
-			Callback = function(Value)
-				Library:ToggleTransparency(Value)
-				Settings.Transparency = Value
-                InterfaceManager:SaveSettings()
-			end
-		})
+                end
+            })
+        end
 
-	end
+        section:AddToggle("TransparentToggle", {
+            Title = "Transparency",
+            Description = "",
+            Default = Settings.Transparency,
+            Callback = function(Value)
+                Library:ToggleTransparency(Value)
+                Settings.Transparency = Value
+                InterfaceManager:SaveSettings()
+            end
+        })
+    end
+end
 
 return InterfaceManager
