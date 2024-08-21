f43f8b623377e11b95b23ea996564bc472cbac58
Tran Van Bao <133487627+TranVanBao1411@users.noreply.github.com>
2024-08-21 21:55:08 +0700

    Update FluentKid.lua

--- FluentKid.lua
+++ FluentKid.lua
@@ -26,7 +26,7 @@ ImageButton.Parent = ScreenGui
 ImageButton.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 ImageButton.BorderSizePixel = 0
 ImageButton.Position = UDim2.new(0.120833337, 0, 0.0952890813, 0)
-ImageButton.Size = UDim2.new(0, 50, 0, 50)
+ImageButton.Size = UDim2.new(0, 47, 0, 47)
 ImageButton.Draggable = true
 ImageButton.Image = "https://www.roblox.com/headshot-thumbnail/image?userId=".. game.Players.LocalPlayer.UserId .."&width=420&height=420&format=png"
 ImageButton.MouseButton1Down:connect(function()
